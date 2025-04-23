209572999307a45aca57232830dc880e37e698b4
Garg9 <124443608+Garg9@users.noreply.github.com>
2025-04-23 19:53:12 +0530

    login.js

--- api/admin/login.js
+++ api/admin/login.js
@@ -5,12 +5,18 @@ module.exports = (req, res) => {
   const viewsDir = path.join(__dirname, '../../views/admin');
   const filePath = path.join(viewsDir, 'admin_login.ejs');
 
-  ejs.renderFile(filePath, {}, {}, (err, str) => {
+  // Pass errMsg, defaulting to null if not provided
+  const data = {
+    errMsg: req.query.error || null, // Can be adjusted based on error source
+  };
+
+  ejs.renderFile(filePath, data, (err, html) => {
     if (err) {
-      res.status(500).send('Error rendering admin login page');
+      console.error('❌ Error rendering admin login page:', err.message, err.stack);
+      res.status(500).send(`Error rendering admin login page: ${err.message}`);
     } else {
       res.setHeader('Content-Type', 'text/html');
-      res.status(200).send(str);
+      res.status(200).send(html);
     }
   });
 };
