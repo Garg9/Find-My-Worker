2818c6df1b4886ab027f768efc7afa277146bb4b
Garg9 <124443608+Garg9@users.noreply.github.com>
2025-04-23 18:22:19 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -3,51 +3,51 @@
 
 // module.exports = serverless(app);
 
-// const ejs = require('ejs');
-// const path = require('path');
-
-// module.exports = (req, res) => {
-//   // Set the path to your 'views' folder
-//   const viewsDir = path.join(__dirname, '..', 'views');
-
-//   // Render the 'index.ejs' file
-//   ejs.renderFile(path.join(viewsDir, 'index.ejs'), {}, (err, html) => {
-//     if (err) {
-//       res.status(500).send('Error rendering the page');
-//     } else {
-//       res.status(200).send(html);
-//     }
-//   });
-// };
-
-const serverless = require('serverless-http');
-const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
-const app = express();
+module.exports = (req, res) => {
+  // Set the path to your 'views' folder
+  const viewsDir = path.join(__dirname, '..', 'views');
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+  // Render the 'index.ejs' file
+  ejs.renderFile(path.join(viewsDir, 'index.ejs'), {}, (err, html) => {
+    if (err) {
+      res.status(500).send('Error rendering the page');
+    } else {
+      res.status(200).send(html);
+    }
+  });
+};
 
-// Home
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// const serverless = require('serverless-http');
+// const express = require('express');
+// const path = require('path');
 
-// Client login
-app.get('/client/login', (req, res) => {
-  res.render('client/clientlogin');
-});
+// const app = express();
 
-// Admin login
-app.get('/admin/login', (req, res) => {
-  res.render('admin/admin_login');
-});
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '../views'));
 
-// Worker login
-app.get('/worker/login', (req, res) => {
-  res.render('worker/workerlogin');
-});
+// // Home
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 
-module.exports = serverless(app);
+// // Client login
+// app.get('/client/login', (req, res) => {
+//   res.render('client/clientlogin');
+// });
+
+// // Admin login
+// app.get('/admin/login', (req, res) => {
+//   res.render('admin/admin_login');
+// });
+
+// // Worker login
+// app.get('/worker/login', (req, res) => {
+//   res.render('worker/workerlogin');
+// });
+
+// module.exports = serverless(app);
 
