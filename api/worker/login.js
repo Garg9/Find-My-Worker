ef4d22ff2f39d21e6ecd1e8b5f079c56bb5999b1
Garg9 <124443608+Garg9@users.noreply.github.com>
2025-04-23 19:31:47 +0530

    login.js

--- api/worker/login.js
+++ api/worker/login.js
@@ -4,9 +4,11 @@ const path = require('path');
 module.exports = (req, res) => {
   const viewsDir = path.join(__dirname, '..', '..', 'views', 'worker');
 
-  ejs.renderFile(path.join(viewsDir, 'workerlogin.ejs'), {}, (err, html) => {
+  const filePath = path.join(viewsDir, 'workerlogin.ejs');
+
+  ejs.renderFile(filePath, {}, (err, html) => {
     if (err) {
-      console.error("EJS Render Error:", err);  // Helpful debug
+      console.error('Error rendering EJS:', err);
       res.status(500).send('Error rendering worker login page');
     } else {
       res.status(200).send(html);
