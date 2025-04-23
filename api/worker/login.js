c8d846c1b7e300b7d9c0bc80badc746090ee583c
Garg9 <124443608+Garg9@users.noreply.github.com>
2025-04-23 19:34:20 +0530

    login.js

--- api/worker/login.js
+++ api/worker/login.js
@@ -2,13 +2,13 @@ const ejs = require('ejs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const viewsDir = path.join(__dirname, '..', '..', 'views', 'worker');
+  const viewsPath = path.join(__dirname, '..', '..', 'views', 'worker');
+  const filePath = path.join(viewsPath, 'workerlogin.ejs');
 
-  const filePath = path.join(viewsDir, 'workerlogin.ejs');
-
+  console.log("Attempting to render:", filePath); // Logs to Vercel
   ejs.renderFile(filePath, {}, (err, html) => {
     if (err) {
-      console.error('Error rendering EJS:', err);
+      console.error('❌ EJS Render Error:', err);
       res.status(500).send('Error rendering worker login page');
     } else {
       res.status(200).send(html);
